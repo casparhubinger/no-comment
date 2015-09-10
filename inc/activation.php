5083ab4e961872239d407a2e839fc0697637443a
Caspar Hübinger <caspar@glueckpress.com>
2015-09-10 20:08:51 +0200

    minor improvements for plugin activation

--- inc/activation.php
+++ inc/activation.php
@@ -6,7 +6,7 @@
  */
 function no_comment__activate() {
 	$message = no_comment__get_activation_message();
-	set_transient( 'no_comment_plugin_activation', $message, 10 );
+	set_transient( 'no_comment_plugin_activation', $message, 5 );
 }
 
 /**
@@ -40,8 +40,8 @@ function no_comment__get_activation_message() {
 	$message  = sprintf( '<p>%s</p>', __( '<strong>No Comment</strong> has been activated and has performed the following operations:', 'no-comment' ) );
 	$message .= '<ul class="ul-square">';
 	$message .= sprintf( '<li>%s</li>', __( 'Set previously published comments to unapproved.', 'no-comment' ) );
-	$message .= sprintf( '<li>%s</li>', __( 'Closed comments on all published posts of all post types.', 'no-comment' ) );
-	$message .= sprintf( '<li>%s</li>', __( 'Set default comment status to closed.', 'no-comment' ) );
+	$message .= sprintf( '<li>%s</li>', __( 'Closed comments on all published posts of all post types once. (Future status handled by global default status.)', 'no-comment' ) );
+	$message .= sprintf( '<li>%s</li>', __( 'Set global default comment status to closed.', 'no-comment' ) );
 	$message .= sprintf( '<li>%s</li>', __( 'Removed comments and discussion menu pages from admin menu.', 'no-comment' ) );
 	$message .= sprintf( '<li>%s</li>', __( 'Removed the comment menu item from admin bar.', 'no-comment' ) );
 	$message .= sprintf( '<li>%s</li>', __( 'Removed post type support for comments from all registered post types.', 'no-comment' ) );
@@ -70,6 +70,7 @@ function no_comment__get_random_message() {
 		__( 'Hey, you’ve nailed this. How about giving yourself a treat?', 'no-comment' ),
 		__( 'Ok, moar coffee now…', 'no-comment' ),
 		__( 'Imagine: Somewhere someone is tearing their hair out over a problem right now, and it’s not you!', 'no-comment' ),
+		__( 'So the plural of “status” is… “status”. You knew that, didn’t you?', 'no-comment' ),
 	);
 	shuffle( $random );
 	$randomized = mt_rand( 0, count( $random ) - 1 );
